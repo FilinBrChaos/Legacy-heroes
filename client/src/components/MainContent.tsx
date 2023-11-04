dace8b4e7586efba6248e2739f3179b4d6543ec7
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2023-11-05 00:29:57 +0200

    added some main page structure

--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -1,10 +1,32 @@
-import { Container } from '@mui/material';
+import { Box, Divider, Typography, Container, Grid } from '@mui/material';
 
 const MainContent = () => {
+  const user = {
+    name: 'Username',
+    work: 'UserWork',
+    phone: '923828374',
+    email: 'example@gmail.com'
+  }
   return (
-    <Container>
-      <h2>My User Profile</h2>
-    </Container>
+    <Grid container sx={{ w: '100%', h: '100%' }}>
+      <Grid item xs={8}>
+        <Box sx={{ pt: '10vh', pl: '4vw' }}>
+          <h3>Directory</h3>
+          <Typography>Share your directory with your whole team to help them get to know each other.</Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box sx={{ backgroundColor: 'gray' }}>
+          <Box display="flex" alignItems="center" flexDirection="column">
+            <Box sx={{ backgroundColor: 'black', minWidth: '100px', minHeight: '100px', maxHeight: '100px', maxWidth: '100px', borderRadius: 999 }}></Box>
+            <Typography>Username</Typography>
+            <Typography>User work</Typography>
+          </Box>
+          <Divider />
+          
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
