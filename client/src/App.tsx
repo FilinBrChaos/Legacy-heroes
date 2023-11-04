9f9227fdab06fcd67cd68326f4c84de8cf83b65d
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2023-11-04 22:30:52 +0200

    new generated frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import Header from './components/Header';
-import MainContent from 'components/MainContent';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Root } from './components/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: () => { return '' },
+  },
+]);
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ client/src/components/Root.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import MainContent from "./MainContent";
+
+export function Root(): JSX.Element {
+    return(
+        <>
+            <Header />
+            <MainContent />
+        </>
+    )
+}
