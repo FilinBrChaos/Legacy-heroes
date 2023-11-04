df0c22f244a51505127d414deab80b222f41d606
Dmytro Khabaznia <dmytro.khabaznia@gmail.com>
2023-11-04 21:29:22 +0200

    Implement getting badges by name

--- api/src/main/java/trainual/userservice/api/controller/BadgeController.java
+++ api/src/main/java/trainual/userservice/api/controller/BadgeController.java
@@ -1,11 +1,16 @@
 package trainual.userservice.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import trainual.userservice.dto.BadgeDto;
 import trainual.userservice.service.BadgeService;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -18,5 +23,15 @@ public class BadgeController {
     @GetMapping("/all")
     public ResponseEntity<List<BadgeDto>> getBadgesList() {
         return ResponseEntity.ok(badgeService.getAllBadges());
+    }
+
+    @GetMapping("/{badgePath}")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> getImageDynamicType(@PathVariable("badgePath") String badgePath) throws IOException {
+        MediaType contentType = MediaType.IMAGE_PNG;
+        InputStream in = new ClassPathResource("badges/" + badgePath).getInputStream();
+        return ResponseEntity.ok()
+                .contentType(contentType)
+                .body(new InputStreamResource(in));
     }
 }
---
+++ api/src/main/resources/sql/insert_test_badges.sql
@@ -0,0 +1,6 @@
+INSERT into badge (badge_id,badge_url) values (1, 'best_start.png');
+INSERT into badge (badge_id,badge_url) values (2, 'excel_connector.png');
+INSERT into badge (badge_id,badge_url) values (3, 'firefighter.png');
+INSERT into badge (badge_id,badge_url) values (4, 'illum.png');
+INSERT into badge (badge_id,badge_url) values (5, 'thank_you.png');
+INSERT into badge (badge_id,badge_url) values (6, 'wellb_amba.png');
